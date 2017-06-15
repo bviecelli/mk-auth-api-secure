1966d734f7283954140039cfb2418d191db9433b
Bruno Viecelli <bviecelli@gmail.com>
2017-06-14 22:45:52 -0300

    PATCH
    Versão funcional completa:
    Dados trafegam criptografados de ponta a ponta através do JWT.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, session, redirect, url_for, escape, request
-# import requests
+import jwt
 import json
 from models import SisResumo, SisLanc, SisCliente, SisMsg, SisSuporte
 from flask_sqlalchemy import SQLAlchemy
@@ -11,7 +11,7 @@ from config import SERVER_ADDR
 
 
 app = Flask(__name__)
-API_KEY = '83fdf5e05ffb52a01f37c22b27e0da07'
+JWT_KEY = 'secret_test'
 
 app_settings = os.getenv(
     'APP_SETTINGS',
@@ -19,6 +19,16 @@ app_settings = os.getenv(
 )
 app.config.from_object(app_settings)
 db = SQLAlchemy(app)
+
+
+def encapsulate_jwt(pkt):
+    data = jwt.encode({"data": pkt}, JWT_KEY, algorithm='HS256').decode()
+    return data
+
+
+def ret_error(err):
+    data = {"error": err}
+    return encapsulate_jwt(data)
 
 
 def mk_login(username, passwd):
@@ -43,85 +53,83 @@ def index():
     if request.method == 'POST':
         logout()
     if 'username' not in session:
-        return 'You are not logged in'
-    return '''
-        Logged in as %s
-    <form method="post">
-        <p><input type=submit value=Logout>
-    </form>
-    ''' % escape(session['username'])
+        return ret_error('You are not logged in')
+    data = {"result": "success", "user": escape(session['username'])}
+    return encapsulate_jwt(data)
 
 
 @app.route('/titulos')
 def titulos():
     if 'username' not in session:
-        return 'You are not logged in'
+        return ret_error('You are not logged in')
     t_list = db_session.query(SisLanc).filter(SisLanc.login == session['username']).all()
 
     bill_list = []
     for itm in t_list:
         bill_list.append(jsonpickle.encode(itm))
     json_ret = json.dumps(bill_list)
-    return json_ret
+    return encapsulate_jwt(json_ret)
 
 
 @app.route('/conta')
 def conta():
     if 'username' not in session:
-        return 'You are not logged in'
+        return ret_error('You are not logged in')
     t_list = db_session.query(SisCliente).filter(SisCliente.login == session['username']).all()
 
     c_list = []
     for itm in t_list:
         c_list.append(jsonpickle.encode(itm))
     json_ret = json.dumps(c_list)
-    return json_ret
+    return encapsulate_jwt(json_ret)
 
 
 @app.route('/resumo')
 def resumo():
     if 'username' not in session:
-        return 'You are not logged in'
+        return ret_error('You are not logged in')
     t_list = db_session.query(SisResumo).filter(SisResumo.login == session['username']).all()
 
     c_list = []
     for itm in t_list:
         c_list.append(jsonpickle.encode(itm))
     json_ret = json.dumps(c_list)
-    return json_ret
+    return encapsulate_jwt(json_ret)
 
 
 @app.route('/msgs')
 def msgs():
     if 'username' not in session:
-        return 'You are not logged in'
+        return ret_error('You are not logged in')
     t_list = db_session.query(SisMsg).filter(SisMsg.login == session['username']).all()
 
     c_list = []
     for itm in t_list:
         c_list.append(jsonpickle.encode(itm))
     json_ret = json.dumps(c_list)
-    return json_ret
+    return encapsulate_jwt(json_ret)
 
 
 @app.route('/chamados')
 def chamados():
     if 'username' not in session:
-        return 'You are not logged in'
+        return ret_error('You are not logged in')
     t_list = db_session.query(SisSuporte).filter(SisSuporte.login == session['username']).all()
 
     c_list = []
     for itm in t_list:
         c_list.append(jsonpickle.encode(itm))
     json_ret = json.dumps(c_list)
-    return json_ret
+    return encapsulate_jwt(json_ret)
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    if mk_login(request.form['username'], request.form['password']):
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
+    if len(request.form['data']) > 0:
+        data = jwt.decode(request.form['data'], JWT_KEY, algorithm='HS256')
+        if mk_login(data['username'], data['password']):
+            session['username'] = data['username']
+            return redirect(url_for('index'))
     return redirect(url_for('login'))
 
 
@@ -157,6 +165,5 @@ if __name__ == '__main__':
     db_session = Session()
     app.run()
 
-# TODO: Trocar a key dos cookies
-# TODO: Trocar login via form para JSON
-# TODO: Utilizar JWT para todo o conteúdo JSON
+# TODO: Trocar a key dos cookies e JWT_KEY para cada instância em produção
+# TODO: Remover rota login_test para o release
